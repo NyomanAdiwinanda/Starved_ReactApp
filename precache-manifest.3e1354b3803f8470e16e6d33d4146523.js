7f0d9441e13b91692eff2b1a0eed7bc16ddbaf22
Nyoman Adiwinanda <nyoman.adi16@gmail.com>
2020-10-03 06:12:45 +0700

    Updates

---
+++ precache-manifest.3e1354b3803f8470e16e6d33d4146523.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab93d84b0a3b65bd578ea3f076f6a5b5",
+    "url": "/Starved_ReactApp/index.html"
+  },
+  {
+    "revision": "a8a14733fe4d32405801",
+    "url": "/Starved_ReactApp/static/css/main.59770e2a.chunk.css"
+  },
+  {
+    "revision": "b325f4f2deebde204f41",
+    "url": "/Starved_ReactApp/static/js/2.030235a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Starved_ReactApp/static/js/2.030235a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8a14733fe4d32405801",
+    "url": "/Starved_ReactApp/static/js/main.3f033c67.chunk.js"
+  },
+  {
+    "revision": "71ac1b5ab786cd4c5ebd",
+    "url": "/Starved_ReactApp/static/js/runtime-main.65f743fb.js"
+  },
+  {
+    "revision": "862f54a94cf127db36a600509e0ccc9b",
+    "url": "/Starved_ReactApp/static/media/background_search_desktop.862f54a9.webp"
+  },
+  {
+    "revision": "082e88ec250194e6416adc8772adea4e",
+    "url": "/Starved_ReactApp/static/media/background_search_mobile.082e88ec.webp"
+  }
+]);
